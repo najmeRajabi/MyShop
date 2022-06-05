63e3229fbced2466ed8f181c81748d885680a407
niha8515 <niha8515@yahoo.com>
2022-06-05 18:37:34 +0430

    add HomeFragment.kt and design it

---
+++ app/src/main/java/com/example/myshop/ui/home/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.myshop.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.example.myshop.R
+import com.example.myshop.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+    val vModel: HomeViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/myshop/ui/home/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.myshop.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+}
