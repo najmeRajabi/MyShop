4ebb66f776bbd3e068876bbd209a0c13e42beb67
niha8515 <niha8515@yahoo.com>
2022-06-05 15:28:15 +0430

    add dependency and nav_graph.xml

--- app/src/main/java/com/example/myshop/ui/MainActivity.kt
+++ app/src/main/java/com/example/myshop/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.myshop
+package com.example.myshop.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myshop.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
