3c5eb68473d0a26079d333297ebcc6923d90b917
niha8515 <niha8515@yahoo.com>
2022-06-07 08:22:24 +0430

    update HomeListsAdapter.kt and add home_list_item.xml

--- app/src/main/java/com/example/myshop/ui/adapters/HomeListsAdapter.kt
+++ app/src/main/java/com/example/myshop/ui/adapters/HomeListsAdapter.kt
@@ -1,4 +1,77 @@
 package com.example.myshop.ui.adapters
 
-class HomeListsAdapter {
+import android.view.LayoutInflater
+import android.view.RoundedCorner
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.example.myshop.R
+import com.example.myshop.model.Product
+
+
+typealias ClickHandler = (product: Product) -> Unit
+
+class HomeListsAdapter(var clickHandler: ClickHandler):
+    ListAdapter<Product, HomeListsAdapter.ViewHolder>(ProductDiffCallback){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeListsAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.home_list_item, parent, false)
+
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HomeListsAdapter.ViewHolder, position: Int) {
+        holder.bind(getItem(position) , clickHandler )
+
+
+    }
+
+    object ProductDiffCallback : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+
+    class ViewHolder( val view: View): RecyclerView.ViewHolder(view) {
+        val txvTitle = view.findViewById<TextView>(R.id.txv_name_list_Home)
+        val imvImage = view.findViewById<ImageView>(R.id.imv_list_home)
+
+
+        fun bind(
+            product: Product,
+            clickHandler: ClickHandler
+        ) {
+
+            txvTitle.text = product.name
+            Glide
+                .with(view)
+                .load(product.images[0].src)
+                .centerCrop()
+                .transition(withCrossFade())
+                .transform(CenterInside() , RoundedCorners(25) )
+                .placeholder(R.drawable.ic_baseline_more_horiz_24)
+                .error(R.drawable.ic_baseline_image_not_supported_24)
+                .into(imvImage)
+
+            view.setOnClickListener { clickHandler(product) }
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/myshop/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myshop/ui/home/HomeFragment.kt
@@ -9,6 +9,10 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.example.myshop.R
 import com.example.myshop.databinding.FragmentHomeBinding
+import com.example.myshop.model.Category
+import com.example.myshop.model.Image
+import com.example.myshop.model.Product
+import com.example.myshop.ui.adapters.HomeListsAdapter
 
 class HomeFragment : Fragment() {
 
@@ -24,6 +28,33 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_home, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+
+
+    }
+
+    private fun initViews() {
+        val lastAdapter=HomeListsAdapter{}
+        val mostSeenAdapter=HomeListsAdapter{}
+        val favoriteAdapter=HomeListsAdapter{}
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.lastAdaptor = lastAdapter
+        binding.mostSeenAdaptor = mostSeenAdapter
+        binding.favoriteAdaptor = favoriteAdapter
+
+        lastAdapter.submitList(arrayListOf(
+            Product(1, arrayListOf(Image(1,"",
+                "https://woocommerce.s3.ir-thr-at1.arvanstorage.com/301185.jpg")),"name",
+                "5454", arrayListOf()),
+            Product(2, arrayListOf(Image(1,"","https://woocommerce.s3.ir-thr-at1.arvanstorage.com/301416.jpg")),"name","5454", arrayListOf()),
+            Product(3, arrayListOf(Image(1,"","https://woocommerce.s3.ir-thr-at1.arvanstorage.com/301302.jpg")),"name","5454", arrayListOf()),
+            Product(4, arrayListOf(Image(1,""," ")),"name","5454", arrayListOf()),
+        ))
     }
 
 }
