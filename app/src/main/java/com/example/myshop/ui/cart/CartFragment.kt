3b5e51425407cd4f37b8d86c35ce0878c225f947
niha8515 <niha8515@yahoo.com>
2022-06-19 22:37:21 +0430

    add CartFragment.kt

---
+++ app/src/main/java/com/example/myshop/ui/cart/CartFragment.kt
@@ -0,0 +1,29 @@
+package com.example.myshop.ui.cart
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.myshop.R
+import com.example.myshop.databinding.FragmentCartBinding
+import com.example.myshop.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CartFragment : Fragment() {
+
+    lateinit var binding: FragmentCartBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_cart, container, false)
+        return binding.root
+    }
+}
