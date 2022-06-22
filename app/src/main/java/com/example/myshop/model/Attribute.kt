320c8610f0d01ec35e20636823f12eb7647305cc
niha8515 <niha8515@yahoo.com>
2022-06-22 20:29:53 +0430

    add data class Attribute.kt , Customer.kt

---
+++ app/src/main/java/com/example/myshop/model/Attribute.kt
@@ -0,0 +1,7 @@
+package com.example.myshop.model
+
+data class Attribute(
+    val id: Int,
+    val name: String,
+    val options: List<String>
+)
---
+++ app/src/main/java/com/example/myshop/model/Customer.kt
@@ -0,0 +1,13 @@
+package com.example.myshop.model
+
+data class Customer(
+
+    val id: String,
+    val email: String,
+    val first_name:String,
+    val last_name: String,
+    val role: String,
+    val username :String,
+    val password: String,
+//    val avatar_url: String
+)
