f8c360afacc9f9633d1d69778e2f0ec05bcd0824
niha8515 <niha8515@yahoo.com>
2022-07-13 17:55:30 +0430

    fix wrong keys in NetworkParams.kt

--- app/src/main/java/com/example/myshop/data/network/NetworkParams.kt
+++ app/src/main/java/com/example/myshop/data/network/NetworkParams.kt
@@ -2,7 +2,7 @@ package com.example.myshop.data.network
 
 class NetworkParams {
     companion object {
-        const val BASE_URL = "http://woocommerce.maktabsharif.ir/wp-json/wc/v3/"
+        const val BASE_URL = "https://woocommerce.maktabsharif.ir/wp-json/wc/v3/"
         //private const val KEY = "ck_4e34eda4882e044742e643e35060ab5234231ab3"
         private const val KEY = "ck_63f4c52da932ddad1570283b31f3c96c4bd9fd6f"
         //private const val SECRET = "cs_28f528089254ed3dc6bc8cbd0ffeb34a52f69547"
